b12231c275277ab89219b28adda950c8f194f1d2
sara-gudeman <sgudeman982@gmail.com>
2015-07-13 22:50:45 -0700

    Removed textarea styling.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -2,9 +2,12 @@
 // https://api.parse.com/1/classes/chatterbox
 var app = {
   server: 'https://api.parse.com/1/classes/chatterbox',
+  friends: [],
+
   init: function() {
-    app.fetch();
+    app.fetch(displayMessages);
   },
+
   send: function(message) {
     $.ajax({
       // This is the url you should use to communicate with the parse API server.
@@ -21,6 +24,7 @@ var app = {
       }
     });   
   },
+
   fetch: function(callback) {
     $.ajax({
       // This is the url you should use to communicate with the parse API server.
@@ -30,50 +34,63 @@ var app = {
       contentType: 'application/json',
       success: function (data) {
         callback(data);
-        console.log('chatterbox: Message sent');
+        console.log('chatterbox: Received messages');
+        // console.log('chatterbox: Message sent');
       },
       error: function (data) {
         // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-        console.error('chatterbox: Failed to send message');
+        console.error('chatterbox: Failed to receive messages');
+        // console.error('chatterbox: Failed to send message');
       }
     });    
   },
+
   clearMessages: function() {
     $('#chats').text('');
   },
+
   addMessage: function(message) {
     $('#chats').append(messageTemplate(message));
   },
 
   addRoom: function(roomname) {
     $('#roomSelect').append('<span>' + roomname + '</span>');
+  },
+
+  addFriend: function(username) {
+    app.friends.push(username);
+  },
+
+  handleSubmit: function(text, roomname) {
+    var message = {
+      username: window.location.search.split('=')[1],
+      text: text,
+      roomname: roomname
+    };
+    app.send(message);
+    //app.addMessage(message);
   }
 
 };
 
 $(document).ready(function() {
-  var getMessages = function(callback) {
-  };
-
-  $('input[type="submit"').on('click', function() {
-    var message = {
-      username: $('input[name="username"]').val(),
-      text: $('textarea[name="text"]').val(),
-      roomname: $('input[name="roomname"]').val()
-    };
-    app.send(message);
+  app.init();
+  //console.log(window.location.search.split('=')[1]);
+  $('#send').on('submit', function() {
+    //alert($('#message').val());
+    app.handleSubmit($('#message').val(), $('#roomName').val())
   });
 
-  var displayMessages = function(data) {
-    _.each(data.results, function(message) {
-      if (message.username !== undefined) {
-        $('#chats').append(messageTemplate(message)); 
-      }
-    });
-  };
+  $('#clearChats').on('click', function() {
+    app.clearMessages();
+  });
+
+  $('#chats').on('click', '.username', function() {
+    app.addFriend($(this).text());
+  });
 
   var update = function() {
-    getMessages(displayMessages);
+    app.fetch(displayMessages);
     setTimeout(update, 5000);
   };
 
@@ -91,13 +108,20 @@ var cleaner = function(templateVariables) {
   return cleaned;
 };
 
+function displayMessages(data) {
+  _.each(data.results, function(message) {
+    if (message.username !== undefined) {
+      $('#chats').append(messageTemplate(message)); 
+    }
+  });
+};
 // Templates
 function messageTemplate(variables) {
   cleaned = cleaner(variables);
-  return '<div class="chat">' +
+  return '<ul class="chat">' +
     '<span class="username">' + cleaned.username + '</span>' +
     '<p class="text">' + cleaned.text +'</p>' +
     '<time class="created" datetime="' + cleaned.createdAt + '">' + cleaned.createdAt+ '</time>' +
     '<time class="updated" datetime="' + cleaned.updatedAt + '">' + cleaned.updatedAt + '</time>' +
-  '</div>'
+  '</ul>'
 };
