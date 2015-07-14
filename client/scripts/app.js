02c16f77e18867d5d645a38a46705cd57c07fb48
sara-gudeman <sgudeman982@gmail.com>
2015-07-13 20:37:03 -0700

    Added send, fetch and clearMessages methods, and added app object

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,7 +1,29 @@
 // YOUR CODE HERE:
 // https://api.parse.com/1/classes/chatterbox
-$(document).ready(function() {
-  var getMessages = function(callback) {
+var app = {
+  server: 'https://api.parse.com/1/classes/chatterbox',
+  init: function() {
+    app.fetch();
+  },
+  send: function(message) {
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: app.server,
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        callback(data);
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message');
+      }
+    });   
+  },
+  fetch: function() {
+
     $.ajax({
       // This is the url you should use to communicate with the parse API server.
       url: 'https://api.parse.com/1/classes/chatterbox',
@@ -16,15 +38,30 @@ $(document).ready(function() {
         // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
         console.error('chatterbox: Failed to send message');
       }
-    });
+    });    
+  },
+  clearMessages: function() {
+    $('#chats').text('');
+  }
+};
+
+$(document).ready(function() {
+  var getMessages = function(callback) {
   };
 
-
+  $('input[type="submit"').on('click', function() {
+    var message = {
+      username: $('input[name="username"]').val(),
+      text: $('textarea[name="text"]').val(),
+      roomname: $('input[name="roomname"]').val()
+    };
+    app.send(message);
+  });
 
   var displayMessages = function(data) {
     _.each(data.results, function(message) {
       if (message.username !== undefined) {
-        $('.messages').append(messageTemplate(message)); 
+        $('#chats').append(messageTemplate(message)); 
       }
     });
   };
